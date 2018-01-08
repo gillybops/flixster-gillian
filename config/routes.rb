ba0e293544d1097882685be7c2fc1d548e0fee41
gillybops <gillybops@optonline.net>
2018-01-08 06:55:07 +0000

    Course Creation now Allowed

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#index'
-
-default_url_options Rails.application.config.action_mailer.default_url_options
+  namespace :instructor do
+    resources :courses, only: [:new, :create, :show]
+  end
 end
