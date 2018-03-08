c03ed9d7a0cfed7a653d2637bfcf98e93e76ca34
gillybops <gillybops@optonline.net>
2018-03-08 02:50:44 +0000

    Dealing with Permissions on Lessons

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,23 +1,18 @@
 class Instructor::LessonsController < ApplicationController
-=begin
   before_action :authenticate_user!
   before_action :require_authorized_for_current_section
-=end    
 
   def new
-    @section = Section.find(params[:section_id])
     @lesson = Lesson.new
   end
    
   def create
-    @section = Section.find(params[:section_id])
     @lesson = current_section.lessons.create(lesson_params)
     redirect_to instructor_course_path(current_section.course)
   end
       
   private
-     
-=begin
+
   def require_authorized_for_current_section
     if current_section.course.user != current_user
       return render plain: 'Unauthorized', status: :unauthorized
@@ -28,7 +23,6 @@ class Instructor::LessonsController < ApplicationController
   def current_section
     @current_section ||= Section.find(params[:section_id])
   end
-=end
 
   def lesson_params
     params.require(:lesson).permit(:title, :subtitle)
