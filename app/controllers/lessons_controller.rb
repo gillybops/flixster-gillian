f29688afe3375d1036d414b848e4f086aca55b34
gillybops <gillybops@optonline.net>
2018-03-29 20:59:03 +0000

    Edited Users Enrollment Feature to view Lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -8,24 +8,16 @@ class LessonsController < ApplicationController
 
   private
    
-=begin
   def require_authorized_for_current_course
     if current_lesson.section.course.user != current_user && 
         !current_user.enrolled_in?(current_lesson.section.course)
     redirect_to course_path(current_lesson.section.course), alert: 'Access will be granted, when you enroll.'
     end
   end
-=end
 
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
 
-  def require_authorized_for_current_course
-    @course = current_lesson.section.course
-    if current_user.enrolled_in?(@course) == false 
-      redirect_to course_path(@course), :alert => 'Please enroll to access lesson?'
-    end
-  end
 end
