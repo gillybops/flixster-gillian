4120a37e0989c74ff446ce21ba914fa0c8f40786
gillybops <gillybops@optonline.net>
2018-07-29 15:47:56 +0000

    Attempting to Update Correct Table

--- db/migrate/20180729153259_add_row_order_position_to_sections.rb
+++ db/migrate/20180729153259_add_row_order_position_to_sections.rb
@@ -1,4 +1,4 @@
-AlterSectionsAddRowOrderPosition < ActiveRecord::Migration[5.1]
+class AddRowOrderPositionToSections < ActiveRecord::Migration[5.1]
   def change
     add_column :sections, :row_order_position, :integer
     add_index :sections, :row_order_position
