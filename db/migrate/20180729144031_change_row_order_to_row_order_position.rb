731a6a507b60ad5047cb5ed3ac9d3ea35abf5833
gillybops <gillybops@optonline.net>
2018-07-29 16:04:20 +0000

    Updating Migrations

--- db/migrate/20180729144031_change_row_order_to_row_order_position.rb
+++
@@ -1,5 +0,0 @@
-class ChangeRowOrderToRowOrderPosition < ActiveRecord::Migration[5.1]
-  def change
-    rename_column :sections, :row_order, :row_order_position
-  end
-end
--- db/migrate/20180729153259_add_row_order_position_to_sections.rb
+++
@@ -1,6 +0,0 @@
-class AddRowOrderPositionToSections < ActiveRecord::Migration[5.1]
-  def change
-    add_column :sections, :row_order_position, :integer
-    add_index :sections, :row_order_position
-  end
-end
