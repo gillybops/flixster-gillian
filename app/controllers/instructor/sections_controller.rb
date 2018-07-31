a0f173d9ec677a593f11a8294861454239150cb3
gillybops <gillybops@optonline.net>
2018-07-31 03:54:15 +0000

    Moved the New Section Form Into A Modal

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -3,10 +3,6 @@ class Instructor::SectionsController < ApplicationController
   before_action :require_authorized_for_current_course, only: [:create]
   before_action :require_authorized_for_current_section, only: [:update]
    
-  def new
-    @section = Section.new
-  end
-  
   def create
     @section = current_course.sections.create(section_params)
     redirect_to instructor_course_path(current_course)
@@ -14,36 +10,32 @@ class Instructor::SectionsController < ApplicationController
 
   def update
     current_section.update_attributes(section_params)
-    render text: "updated"
+    render plain: 'ok!'
   end
 
   private
 
   def require_authorized_for_current_section
     if current_section.course.user != current_user
-      render text:  "Unauthorized", status: :unauthorized
+      render plain:  "Unauthorized", status: :unauthorized
     end
   end
    
+  def current_section
+    @current_section ||= Section.find(params[:id])
+  end
+  
   def require_authorized_for_current_course
     if current_course.user != current_user
-      render text:  "Unauthorized", status: :unauthorized
+      render plain:  "Unauthorized", status: :unauthorized
     end
   end
    
   helper_method :current_course
   def current_course
-    if params[:course_id]
-      @current_course ||= Course.find(params[:course_id])
-    else
-      current_section.course
-    end
+    @current_course ||= Course.find(params[:course_id])
   end
 
-  def current_section
-    @current_section ||= Section.find(params[:id])
-  end
-  
   def section_params
     params.require(:section).permit(:title, :row_order_position)
   end
